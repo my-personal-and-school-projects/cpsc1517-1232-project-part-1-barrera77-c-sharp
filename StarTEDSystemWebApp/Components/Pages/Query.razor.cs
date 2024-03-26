d8cb34f8e8bebb88b41be2facf9d90261191e25a
Manuel Alva <barrera_ml@hotmail.com>
2024-03-26 00:36:49 -0600

    Load Programs in the <select>

--- StarTEDSystemWebApp/Components/Pages/Query.razor.cs
+++ StarTEDSystemWebApp/Components/Pages/Query.razor.cs
@@ -1,6 +1,43 @@
-﻿namespace StarTEDSystemWebApp.Components.Pages
+﻿using Microsoft.AspNetCore.Components;
+using StarTEDSystemDB.BLL;
+using StarTEDSystemDB.Entities;
+
+namespace StarTEDSystemWebApp.Components.Pages
 {
     public partial class Query
     {
+        [Inject]
+        CourseServices CourseServices { get; set; }
+
+        [Inject]
+        ProgramServices ProgramServices { get; set; }
+
+        [Inject]
+        ProgramCourseServices ProgramCourseServices { get; set; }
+
+        //Required properties
+        public List<StarTEDSystemDB.Entities.Program> Programs { get; set; } 
+        public List<Course> Course { get; set; }
+
+        public List<string> errorList = new List<string>();
+        public string feedback { get; set; }
+
+        [Parameter]
+        public int ProgramId { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return Task.Run(() =>
+            {
+                Programs = ProgramServices.GetAllPrograms();
+
+                if (ProgramId != 0)
+                {
+                }
+            });
+        }
+
+
+
     }
 }
