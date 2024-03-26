c54ae008b535593ab8c47110d2b5c5e91c97b034
Manuel Alva <barrera_ml@hotmail.com>
2024-03-25 22:13:44 -0600

    Initial Commit - Setup Project and DB

---
+++ StarTEDSystemDB/BLL/CourseServices.cs
@@ -0,0 +1,24 @@
+﻿using StarTEDSystemDB.DAL;
+using StarTEDSystemDB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarTEDSystemDB.BLL
+{
+    public class CourseServices
+    {
+        private readonly StarTEDContext _context;
+
+        internal CourseServices( StarTEDContext context )
+        {
+            _context = context;
+        }
+
+        public List<Course>GetAllCourses()
+        {
+            return _context.Courses.ToList<Course>();
+        }
+    }
+}
---
+++ StarTEDSystemDB/BLL/ProgramCourseServices.cs
@@ -0,0 +1,21 @@
+﻿using StarTEDSystemDB.DAL;
+using StarTEDSystemDB.Entities;
+
+
+namespace StarTEDSystemDB.BLL
+{
+    public class ProgramCourseServices
+    {
+        private readonly StarTEDContext _context;
+
+        internal ProgramCourseServices(StarTEDContext context)
+        {
+            _context = context;
+        }
+
+        public List<ProgramCourse> GetAllProgramCourses()
+        {
+            return _context.ProgramCourses.ToList<ProgramCourse>();
+        }
+    }
+}
